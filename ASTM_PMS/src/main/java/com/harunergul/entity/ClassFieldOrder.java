1e39755b23db7e521939c554cb69dbbb9c42f635
Harun ERGUL <harunergul@outlook.com>
2021-01-05 16:54:49 +0300

    Util created for ordering fields

---
+++ ASTM_PMS/src/main/java/com/harunergul/entity/ClassFieldOrder.java
@@ -0,0 +1,17 @@
+package com.harunergul.entity;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 
+ * @author Harun ERGUL
+ * @date Jan 5, 2021
+ */
+public class ClassFieldOrder {
+
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface FieldOrder {
+		int value();
+	}
+}
---
+++ ASTM_PMS/src/main/java/com/harunergul/utils/Util.java
@@ -0,0 +1,49 @@
+package com.harunergul.utils;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import com.harunergul.entity.ClassFieldOrder.FieldOrder;
+import com.harunergul.entity.Order;
+
+public class Util<T> {
+
+	public static String[] getFieldsInOrder(Class<?> clazz) {
+		Field[] fields = clazz.getDeclaredFields();
+
+		Arrays.sort(fields, new Comparator<Field>() {
+			@Override
+			public int compare(Field o1, Field o2) {
+				FieldOrder or1 = o1.getAnnotation(FieldOrder.class);
+				FieldOrder or2 = o2.getAnnotation(FieldOrder.class);
+
+				if (or1 != null && or2 != null) {
+					return or1.value() - or2.value();
+				} else if (or1 != null && or2 == null) {
+					return -1;
+				} else if (or1 == null && or2 != null) {
+					return 1;
+				}
+				return o1.getName().compareTo(o2.getName());
+			}
+		});
+
+		String orderedFields[] = new String[fields.length];
+		int i = 0;
+		for (Field field : fields) {
+			orderedFields[i] = field.getName();
+		}
+		return orderedFields;
+	}
+
+	public static void main(String... args) {
+		Order order = new Order();
+		order.setAction_code("abc");
+		order.setBiomaterial("ABCDEF");
+		String[] fieldsss = Util.getFieldsInOrder(Order.class);
+		System.out.println(fieldsss);
+
+	}
+
+}
