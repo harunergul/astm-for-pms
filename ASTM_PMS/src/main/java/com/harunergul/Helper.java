c80ca1e8071909c781dc9b2354f68daf8175cdf8
Harun ERGUL <harunergul@outlook.com>
2021-01-13 10:35:02 +0300

    Adding order related sample requests to the helper

--- ASTM_PMS/src/main/java/com/harunergul/Helper.java
+++ ASTM_PMS/src/main/java/com/harunergul/Helper.java
@@ -9,13 +9,51 @@ import com.harunergul.entity.Order;
 import com.harunergul.entity.Patient;
 import com.harunergul.entity.Record;
 import com.harunergul.entity.types.OrderActionCode;
+import com.harunergul.entity.types.OrderPriorityAction;
 import com.harunergul.entity.types.OrderReportType;
 import com.harunergul.utils.Codec;
 
 public class Helper {
 
-	public String createOrder() {
+	public ArrayList<String> createOrderWithEmergency() {
 
+		Header header = new Header();
+		header.setTimestamp(new Date());
+		header.setSender("ASTM-Host");
+
+		Order order = new Order();
+		order.setAction_code(OrderActionCode.ADD_ORDER);
+		order.setReport_type(OrderReportType.ORDER_RECORD);
+		order.setSample_id("165666");
+		order.setPriority(OrderPriorityAction.ACIL_ISTEM_STAT);
+
+		Patient patient = new Patient();
+		patient.setSeq(1);
+		patient.setLaboratory_id("204092702");
+		patient.setName("ÖZGE", "ŞEVVAL");
+		patient.setSex("F");
+		patient.setAdmission_date(new Date());
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.set(Calendar.YEAR, 1980);
+		patient.setBirthdate(cal.getTime());
+
+		ArrayList<String> tests = new ArrayList<String>();
+		tests.add("10182");
+
+		order.setTest(tests);
+
+		ArrayList<Record> records = new ArrayList<Record>();
+		records.add(header);
+		records.add(patient);
+		records.add(order);
+
+		Codec codec = new Codec();
+		return codec.encode(records, null, null, 1);
+	}
+
+	public ArrayList<String> createOrder() {
 		Header header = new Header();
 		header.setTimestamp(new Date());
 		header.setSender("ASTM-Host");
@@ -28,32 +66,29 @@ public class Helper {
 		Patient patient = new Patient();
 		patient.setSeq(1);
 		patient.setLaboratory_id("204092702");
-		patient.setName("ÖZGE","ŞEVVAL");
+		patient.setName("ÖZGE", "ŞEVVAL");
 		patient.setSex("F");
 		patient.setAdmission_date(new Date());
-		
-		
+
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(new Date());
 		cal.set(Calendar.YEAR, 1980);
 		patient.setBirthdate(cal.getTime());
-		
+
 		ArrayList<String> tests = new ArrayList<String>();
-		tests.add("14755");
-		tests.add("3754");
-		tests.add("375441");
+		tests.add("717");
+		tests.add("20600");
+		tests.add("29070");
 		order.setTest(tests);
 
 		ArrayList<Record> records = new ArrayList<Record>();
 		records.add(header);
 		records.add(patient);
 		records.add(order);
-		
 
 		Codec codec = new Codec();
-		System.out.println(codec.encode(records, null, null, 1));
+		return codec.encode(records, null, null, 1);
 
-		return null;
 	}
 
 	public String repeatOrder() {
@@ -93,7 +128,6 @@ public class Helper {
 		order.setAction_code(OrderActionCode.CANCEL_TEST);
 		order.setReport_type(OrderReportType.CORRECTION);
 		order.setSample_id("11703100");
-		
 
 		ArrayList<String> tests = new ArrayList<String>();
 		tests.add("14755");
@@ -109,5 +143,116 @@ public class Helper {
 		System.out.println(codec.encode(records, null, null, 1));
 
 		return null;
+	}
+
+	public ArrayList<String> deleteOrder() {
+		Header header = new Header();
+		header.setTimestamp(new Date());
+		header.setSender("ASTM-Host");
+
+		Order order = new Order();
+		order.setAction_code(OrderActionCode.CANCEL_TEST);
+		order.setReport_type(OrderReportType.ORDER_RECORD);
+		order.setSample_id("11703100");
+
+		Patient patient = new Patient();
+		patient.setSeq(1);
+		patient.setLaboratory_id("204092702");
+		patient.setName("ÖZGE", "ŞEVVAL");
+		patient.setSex("F");
+		patient.setAdmission_date(new Date());
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.set(Calendar.YEAR, 1980);
+		patient.setBirthdate(cal.getTime());
+
+		ArrayList<String> tests = new ArrayList<String>();
+		tests.add("10182");
+
+		order.setTest(tests);
+
+		ArrayList<Record> records = new ArrayList<Record>();
+		records.add(header);
+		records.add(patient);
+		records.add(order);
+
+		Codec codec = new Codec();
+		return codec.encode(records, null, null, 1);
+	}
+
+	public ArrayList<String> rerunOrder() {
+		Header header = new Header();
+		header.setTimestamp(new Date());
+		header.setSender("ASTM-Host");
+
+		Order order = new Order();
+		order.setAction_code(OrderActionCode.ADD_ORDER);
+		order.setReport_type(OrderReportType.CORRECTION);
+		order.setLaboratory_field_2("R");
+		order.setSample_id("11703100");
+
+		Patient patient = new Patient();
+		patient.setSeq(1);
+		patient.setLaboratory_id("204092702");
+		patient.setName("ÖZGE", "ŞEVVAL");
+		patient.setSex("F");
+		patient.setAdmission_date(new Date());
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.set(Calendar.YEAR, 1980);
+		patient.setBirthdate(cal.getTime());
+
+		ArrayList<String> tests = new ArrayList<String>();
+		tests.add("717");
+
+		order.setTest(tests);
+
+		ArrayList<Record> records = new ArrayList<Record>();
+		records.add(header);
+		records.add(patient);
+		records.add(order);
+
+		Codec codec = new Codec();
+		return codec.encode(records, null, null, 1);
+	}
+
+	public ArrayList<String> rerunDilutionOrder() {
+		Header header = new Header();
+		header.setTimestamp(new Date());
+		header.setSender("ASTM-Host");
+
+		Order order = new Order();
+		order.setPriority(OrderPriorityAction.ACIL_ISTEM_STAT);
+		order.setAction_code(OrderActionCode.ADD_ORDER);
+		order.setReport_type(OrderReportType.CORRECTION);
+		order.setLaboratory_field_2("R");
+		order.setSample_id("11703100");
+
+		Patient patient = new Patient();
+		patient.setSeq(1);
+		patient.setLaboratory_id("204092702");
+		patient.setName("ÖZGE", "ŞEVVAL");
+		patient.setSex("F");
+		patient.setAdmission_date(new Date());
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.set(Calendar.YEAR, 1980);
+		patient.setBirthdate(cal.getTime());
+
+		ArrayList<String> tests = new ArrayList<String>();
+		tests.add("717^..100");
+
+		order.setTest(tests);
+
+		ArrayList<Record> records = new ArrayList<Record>();
+		records.add(header);
+		records.add(patient);
+		records.add(order);
+
+		Codec codec = new Codec();
+		return codec.encode(records, null, null, 1);
 	}
 }
